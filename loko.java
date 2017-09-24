87c288e09b938ec10a2cc3960cbfde404b8bbde8
natwynbeta <32232848+natwynbeta@users.noreply.github.com>
2017-09-24 00:27:45 -0300

    Add files via upload

---
+++ loko.java
@@ -0,0 +1,10 @@
+package nada;
+
+public class Nada {
+
+    public static void main(String[] args) {
+        String TT=("Como voces estao seu pau no cu");
+        System.out.print(TT);
+    }
+    
+}
